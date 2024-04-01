6d7fcb635a2b99a20428b87d63a55370fac5629a
Alade C. Yessoufou <aladecharaf23@gmail.com>
2024-04-01 15:32:25 +0100

    first commit

---
+++ start/routes.ts
@@ -0,0 +1,10 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
