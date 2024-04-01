6e6c266b6f388c50b47592d0e5f3c2b3990bb237
Alade C. Yessoufou <aladecharaf23@gmail.com>
2024-04-01 18:10:27 +0100

    setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: false } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    react(),
+    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+  ],
 
   /**
    * Define aliases for importing modules from
