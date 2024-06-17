1ad496f44ef56bea69466580d52af6f295bb4bdc
Alade Yessoufou <aladecharaf23@gmail.com>
2024-06-17 08:03:10 +0100

    ref: improve the error message component

--- inertia/components/error_message.tsx
+++ inertia/components/error_message.tsx
@@ -1,11 +1,26 @@
 import { InfoIcon } from 'lucide-react'
 
-export function ErrorMessage(props: { message?: string }) {
-  const { message } = props
+export function ErrorMessage<T = Record<string, string>>(props: {
+  errors: T
+  name: keyof T
+  message?: string
+}) {
+  const { message, name, errors } = props
+  if (!(name in (typeof errors as any))) {
+    throw new Error(`Error: ${name as string} can't find this field in the form field`)
+  }
+
   return (
-    <small className="flex items-center gap-x-1 text-xs font-normal text-destructive">
-      <InfoIcon className="w-4 h-4" />
-      {message}
-    </small>
+    <>
+      {errors[name] && (
+        <small
+          id={`${name as string}-error`}
+          className="flex items-center gap-x-1 text-xs font-normal text-destructive"
+        >
+          <InfoIcon className="w-4 h-4" />
+          {message}
+        </small>
+      )}
+    </>
   )
 }
